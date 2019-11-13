b78af62808a40a602c08cebf5f3617445cc4b0b1
EvanCaplan31 <evan@bwssolutions.com>
2019-11-12 19:50:32 -0500

    set up basic authentication that comes prior to all requests

---
+++ authenticate.go
@@ -0,0 +1,31 @@
+package dog
+
+import (
+	"context"
+
+	"github.com/godo"
+	"golang.org/x/oauth2"
+)
+
+type Credentials struct {
+	AccesToken string
+}
+
+func (c *Credentials) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: c.AccesToken,
+	}
+	return token, nil
+}
+
+func authenticate(token *oauth2.Token) (*godo.Client) {
+
+	tokenSource := &Credentials{
+		AccesToken: token.AccessToken,
+	}
+
+	oauthClient := oauth2.NewClient(oauth2.NoContext, tokenSource)
+	client := godo.NewClient(oauthClient)
+
+	return client
+}
