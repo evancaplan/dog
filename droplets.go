b6447c535854c670a38dbefff5b0629d88afa2e1
EvanCaplan31 <evan@bwssolutions.com>
2019-11-25 17:52:08 -0500

    implement two gets in droplets

--- droplets.go
+++ droplets.go
@@ -2,6 +2,8 @@ package dog
 
 import (
 	"context"
+	"errors"
+	"strconv"
 
 	"github.com/digitalocean/godo"
 )
@@ -11,8 +13,13 @@ type GetAllDropletsRequest struct {
 	PerPage int
 	Pat     string
 }
- 
-func GetAllDroplets(gar GetAllDropletsRequest) *[]godo.Droplet {
+
+type FindDropletRequest struct {
+	ID  int
+	Pat string
+}
+
+func GetAllDroplets(gar GetAllDropletsRequest) (*[]godo.Droplet, error) {
 
 	opt := &godo.ListOptions{
 		Page:    gar.Page,
@@ -24,9 +31,21 @@ func GetAllDroplets(gar GetAllDropletsRequest) *[]godo.Droplet {
 
 	droplets, _, err := c.Droplets.List(ctx, opt)
 	if err != nil {
-
+		return nil, errors.New("Unable to get all databases. Godo error: " + err.Error())
 	}
 
-	return &droplets
+	return &droplets, nil
 }
 
+func GetDropletById(fdr FindDropletRequest) (*godo.Droplet, error) {
+
+	c := Authenticate(fdr.Pat)
+	ctx := context.TODO()
+
+	droplet, _, err := c.Droplets.Get(ctx, fdr.ID)
+	if err != nil {
+		return nil, errors.New("Droplet with id: " + strconv.Itoa(fdr.ID) + ", was not found. Godo error: " + err.Error())
+	}
+
+	return droplet, nil
+}
