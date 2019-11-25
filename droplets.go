2e13c5d00a2301707ffb8ecc0e32c84875b48abc
EvanCaplan31 <evan@bwssolutions.com>
2019-11-25 18:51:25 -0500

    add create droplet functionality

--- droplets.go
+++ droplets.go
@@ -8,6 +8,22 @@ import (
 	"github.com/digitalocean/godo"
 )
 
+type CreateDropletRequest struct {
+	Name string
+	Region
+	DropletSize
+	Image             string
+	SSHKeys           []int
+	Backups           bool
+	IPv6              bool
+	Configuration     string
+	PrivateNetworking bool
+	Volumes           []string
+	Tags              []string
+	VPCUUID           string
+	Pat               string
+}
+
 type GetAllDropletsRequest struct {
 	Page    int
 	PerPage int
@@ -48,4 +64,56 @@ func GetDropletById(fdr FindDropletRequest) (*godo.Droplet, error) {
 	}
 
 	return droplet, nil
+}
+
+func CreateDroplet(cdr CreateDropletRequest) (*godo.Droplet, error) {
+
+	keys := createGodoSSHKeys(cdr.SSHKeys)
+	volumes := createVolumes(cdr.Volumes)
+
+	create := &godo.DropletCreateRequest{
+		Name:   cdr.Name,
+		Region: cdr.Region.String(),
+		Size:   cdr.DropletSize.String(),
+		Image: godo.DropletCreateImage{
+			Slug: cdr.Image,
+		},
+		SSHKeys:           keys,
+		IPv6:              cdr.IPv6,
+		UserData:          cdr.Configuration,
+		PrivateNetworking: cdr.PrivateNetworking,
+		Volumes:           volumes,
+		Tags:              cdr.Tags,
+		VPCUUID:           cdr.VPCUUID,
+	}
+
+	c := Authenticate(cdr.Pat)
+	ctx := context.TODO()
+
+	droplet, _, err := c.Droplets.Create(ctx, create)
+	if err != nil {
+		return nil, errors.New("Unable to create droplet. Godo error: " + err.Error())
+	}
+
+	return droplet, nil
+}
+
+func createGodoSSHKeys(keys []int) []godo.DropletCreateSSHKey {
+	var godoKeys []godo.DropletCreateSSHKey
+
+	for i := 1; i < len(keys); i++ {
+		key := godo.DropletCreateSSHKey{ID: keys[i]}
+		godoKeys = append(godoKeys, key)
+	}
+	return godoKeys
+}
+
+func createVolumes(volumes []string) []godo.DropletCreateVolume {
+	var godoVolumes []godo.DropletCreateVolume
+
+	for i := 1; i < len(volumes); i++ {
+		volume := godo.DropletCreateVolume{ID: volumes[i]}
+		godoVolumes = append(godoVolumes, volume)
+	}
+	return godoVolumes
 }
