a7ad7e716099d2392d976e107b28746d45963290
EvanCaplan31 <evan@bwssolutions.com>
2019-11-30 12:44:12 -0500

    remove stringify

---
+++ database_test.go
@@ -0,0 +1,13 @@
+package dog_test
+
+import (
+	"gitlab.com/bwss/dog"
+	"github.com/digitalocean/godo"
+	"testing"
+)
+
+func TestCreateDatabaseCluster(t *testing.T){
+	if (false) {
+		t.Error("ypt")
+	}
+}
