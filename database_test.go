facbacdfa055c090c0da5019fe129b608a375b37
EvanCaplan31 <evan@bwssolutions.com>
2019-11-30 12:33:00 -0500

    set up tests in wrong package

---
+++ database_test.go
@@ -0,0 +1,18 @@
+package dog_test
+
+import (
+	"github.com/digitalocean/godo"
+	"testing"
+
+	"gitlab.com/bwss/dog"
+)
+
+const TestPAT string = "TestPAT"
+
+func TestCreateDatabaseCluster(t *testing.T) {
+	testReq := dog.C{
+		Pat: TestPAT,
+	}
+
+	reti
+}
